91b1fabb0738faa19dd7c49639a135843944066c
ConnectedSensorsPedroDrago <pedro.drago@connectedsensors.ca>
2023-09-05 13:16:51 -0300

    14th commit

--- src/components/Input50PercentWithTitle/page.js
+++ src/components/Input50PercentWithTitle/page.js
@@ -3,24 +3,7 @@ const Input50PercentWithTitle = ({name, setter, placeholder, type, disabled}) =>
     <div className="flex flex-col justify-start w-full md:w-[49%]">
         <p className="text-sm font-medium">{name}</p>
         <input 
-          className={`w-full input-small ${ 
-                (
-                  (
-                    !placeholder.startsWith("Add")
-                    && 
-                    !placeholder.startsWith("Repeat")
-                  )
-                    ||
-                  (
-                    (name === "Name") 
-                    || 
-                    (name === "Surname") 
-                    || 
-                    (disabled === true)
-                  )
-                )   
-
-            &&  `text-dark-grey font-semibold`}`}
+          className={`w-full input-small ${disabled === true && `disabled:text-dark-grey disabled:bg-white font-semibold`}`}
           onChange={(e)=> setter(e.target.value !== undefined ? e.target.value : undefined)} 
           placeholder={placeholder} 
           type={type ? type : "text"}
