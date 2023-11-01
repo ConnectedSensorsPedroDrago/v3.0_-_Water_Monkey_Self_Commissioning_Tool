29d59d27b366bae689a7e058707b1697d03e8b06
ConnectedSensorsPedroDrago <pedro.drago@connectedsensors.ca>
2023-11-01 17:01:23 -0300

    38th commit

--- src/app/(application)/comm-tool/step-3/[id]/page.js
+++ src/app/(application)/comm-tool/step-3/[id]/page.js
@@ -47,7 +47,7 @@ const Step3 = ({params}) => {
         console.log(payload)
         try{
             console.log(params.id)
-            let response = await fetch(`https://industrial.api.ubidots.com/api/v2.0/devices/~${params.id}/`, {
+            let response = await fetch(`https://cs.api.ubidots.com/api/v2.0/devices/~${params.id}/`, {
                 method: 'PATCH',
                 headers:{
                     'Content-Type':'application/json',
@@ -55,14 +55,17 @@ const Step3 = ({params}) => {
                 },
                 body: JSON.stringify({
                     "properties": {
-                        "commission_stage": JSON.parse({
-                            first: payload,
-                            second: commStage.second
+                        "commission_stage": JSON.stringify({
+                            "first": payload,
+                            "second": commStage.second
                         })
                     }
                 })
             })
-            let data = response.json()
+            let data = await response.json()
+            if(data.label === params.id){
+                setCommStage(JSON.parse(data.properties.commission_stage))
+            }
             console.log(data)
         }catch(e){
             setError("There was an error writting the first readings: " + e + ". Please try again or contact support.")
@@ -84,19 +87,24 @@ const Step3 = ({params}) => {
                 },
                 body: JSON.stringify({
                     "properties": {
-                        "commission_stage": JSON.parse({
-                            first: commStage.first,
-                            second: payload
+                        "commission_stage": JSON.stringify({
+                            "first": commStage.first,
+                            "second": payload
                         })
                     }
                 })
             })
-            let data = response.json()
+            let data = await response.json()
+            if(data.label === params.id){
+                setCommStage(JSON.parse(data.properties.commission_stage))
+            }
             console.log(data)
         }catch(e){
             setError("There was an error writting the first readings: " + e + ". Please try again or contact support.")
         }
     }
+
+    console.log(commStage)
 
   return (
     <div className='container-pages h-fit'>
@@ -134,26 +142,29 @@ const Step3 = ({params}) => {
         <h1 className="text-[1.5rem] lg:text-[3.25rem] font-bold text-center text-blue-hard mb-[1.5rem] md:mb-[1.5rem]">After successful install...</h1>
         <div className='w-full md:w-[90%] flex md:flex-row flex-col items-start justify-center'>
             <div className='w-full flex flex-col'>
-                <p className='text-dark-grey font-bold text-[1.2rem] md:text-[1.5rem] mb-[1rem]'>Enter initial meter readings</p>
+                <p className={`${commStage && commStage.first.date_time ? `text-grey` : `text-dark-grey`} font-bold text-[1.2rem] md:text-[1.5rem] mb-[1rem]`}>Enter initial meter readings</p>
                 <InputFullPercentWithTitle 
                     name={"Date and Time"}
                     type={"datetime-local"}
-                    placeholder={""}
+                    placeholder={commStage && commStage.first.date_time ? commStage.first.date_time : ""}
                     setter={setDateFirst}
+                    disabled={commStage && commStage.first.date_time ? true : false}
                 />
                 <InputFullPercentWithTitle 
                     name={meterType === "Single" ? "Meter Reading" : "Low Side Meter Reading"}
                     type={"number"}
-                    placeholder={""}
+                    placeholder={commStage && commStage.first.low ? commStage.first.low : ""}
                     setter={setLowSideFirst}
+                    disabled={commStage && commStage.first.date_time ? true : false}
                 />
                 {
                     meterType === "Compound" &&
                     <InputFullPercentWithTitle 
                         name={"High Side Meter Reading"}
                         type={"number"}
-                        placeholder={""}
+                        placeholder={commStage && commStage.first.high ? commStage.first.high : ""}
                         setter={setHighSideSecond}
+                        disabled={commStage && commStage.first.date_time ? true : false}
                     />
                 }
                 <InputFullPercentWithTitle 
@@ -161,29 +172,32 @@ const Step3 = ({params}) => {
                     type={"file"}
                     placeholder={"Select File"}
                     setter={setPicFirst}
+                    disabled={commStage && commStage.first.date_time ? true : false}
                 />
-                <button 
-                    className=" md:mt-0 w-full button-small text-[1rem] h-[2.5rem]"
-                    onClick={()=> onSubmitFirst()}
-                >
-                    Submit
-                </button>
+                {   commStage && !commStage.first.date_time &&
+                    <button 
+                        className=" md:mt-0 w-full button-small text-[1rem] h-[2.5rem]"
+                        onClick={()=> onSubmitFirst()}
+                    >
+                        Submit
+                    </button>
+                }
             </div>
             <div className='w-full flex flex-col md:ml-[1rem] md:mt-0 mt-[2rem]'>
-                <p className='text-grey font-bold text-[1.2rem] md:text-[1.5rem] mb-[1rem]'>Enter final meter readings</p>
+                <p className={`${commStage && !commStage.second.date_time && commStage.first.date_time ? `text-dark-grey` : `text-grey`} font-bold text-[1.2rem] md:text-[1.5rem] mb-[1rem]`}>Enter final meter readings</p>
                 <InputFullPercentWithTitle 
                     name={"Date and Time"}
                     type={"datetime-local"}
                     placeholder={""}
                     setter={setDateSecond}
-                    disabled={true}
+                    disabled={commStage && !commStage.second.date_time && commStage.first.date_time ? false : true}
                 />
                 <InputFullPercentWithTitle 
                     name={meterType === "Single" ? "Meter Reading" : "Low Side Meter Reading"}
                     type={"number"}
                     placeholder={""}
                     setter={setLowSideSecond}
-                    disabled={true}
+                    disabled={commStage && !commStage.second.date_time && commStage.first.date_time ? false : true}
                 />
                 {
                     meterType === "Compound" &&
@@ -192,7 +206,7 @@ const Step3 = ({params}) => {
                         type={"number"}
                         placeholder={""}
                         setter={setHighSideSecond}
-                        disabled={true}
+                        disabled={commStage && !commStage.second.date_time && commStage.first.date_time ? false : true}
                     />
                 }
                 <InputFullPercentWithTitle 
@@ -200,14 +214,16 @@ const Step3 = ({params}) => {
                     type={"file"}
                     placeholder={"Select File"}
                     setter={setPicSecond}
-                    disabled={true}
+                    disabled={commStage && !commStage.second.date_time && commStage.first.date_time ? false : true}
                 />
-                <button 
-                    className="hidden md:mt-0 w-full button-small text-[1rem] h-[2.5rem]"
-                    onClick={()=> onSubmitSecond()}
-                >
-                    Submit
-                </button>
+                {   commStage && !commStage.second.date_time && commStage.first.date_time &&
+                    <button 
+                        className="md:mt-0 w-full button-small text-[1rem] h-[2.5rem]"
+                        onClick={()=> onSubmitSecond()}
+                    >
+                        Submit
+                    </button>
+                }
             </div>
         </div>
     </div>
--- src/components/InputFullPercentWithTitle/page.js
+++ src/components/InputFullPercentWithTitle/page.js
@@ -1,7 +1,7 @@
 const InputFullPercentWithTitle = ({name, setter, placeholder, type, disabled}) => {
   return (
     <div className="flex flex-col justify-start w-full">
-        <p className="text-sm font-medium">{name}</p>
+        <p className={`${disabled ? 'text-grey' : 'text-dark-grey'} text-sm font-medium`}>{name}</p>
         <input 
           className={`w-full input-small ${type === ("file" || "datetime-local") ? `disabled:text-grey` : disabled === true && `disabled:text-grey disabled: disabled:bg-white font-semibold`}`}
           onChange={(e)=> setter(e.target.value !== undefined ? e.target.value : undefined)} 
