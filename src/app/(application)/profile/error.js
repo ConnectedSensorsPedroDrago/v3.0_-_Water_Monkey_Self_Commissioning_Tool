529c1aaa0dea0167ed25e60bb1946f8d17471fd6
ConnectedSensorsPedroDrago <pedro.drago@connectedsensors.ca>
2024-03-05 16:22:41 -0300

    126th commit

---
+++ src/app/(application)/profile/error.js
@@ -0,0 +1,14 @@
+"use client"
+const Error = ({ params }) => {
+
+  return (
+    <div className='container-dashboard bg-grey-light z-0'>
+      <p className="error-message">
+        There has been an error, please refresh the page.
+      </p>
+    </div>
+    
+  )
+}
+
+export default Error
