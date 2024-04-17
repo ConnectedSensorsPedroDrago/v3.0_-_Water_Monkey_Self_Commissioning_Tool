f00f4328729c1af9328519af73800b2995b4e19d
ConnectedSensorsPedroDrago <pedro.drago@connectedsensors.ca>
2024-04-17 16:00:03 -0300

    8th commit

--- src/app/api/devices/water-monkey/get-device-with-description/route.js
+++ src/app/api/devices/water-monkey/get-device-with-description/route.js
@@ -3,7 +3,7 @@ export async function GET(req){
     let code = req.nextUrl.searchParams.get("code")
 
     try{
-        let response = await fetch(`https://cs.api.ubidots.com/api/v2.0/devices/?description=${code}`, {
+        let response = await fetch(`https://cs.api.ubidots.com/api/v2.0/devices/?description=${code.toUpperCase()}`, {
             method: 'GET',
             headers:{
                 'Content-Type':'application/json',
@@ -11,14 +11,13 @@ export async function GET(req){
             }
         })
         let data = await response.json()
-        if(data.results && data.results[0].description == code){
+        if(data.results && data.results[0] && data.results[0].description == code.toUpperCase()){
             return new Response(JSON.stringify({"status": "ok", "device": data.results[0]}))
-        } else {
-            return new Response(JSON.stringify({"status": "error", "message": "There was an error requesting the WM from the QR Code. Please try again or contact support"}))
+        } else if(data.count == 0) {
+            return new Response(JSON.stringify({"status": "error", "message": `We could not find a Water Monkey with the QR Code you submitted (${code.toUpperCase()}). Please try again or contact support`}))
         }
     }catch(e){
-        return new Response(JSON.stringify({"status": "error", "message": "There was an error requesting the WM from the QR Code. Please try again or contact support"}))
+        return new Response(JSON.stringify({"status": "error", "message": `There was an error requesting the WM from the QR Code: ${e}. Please try again or contact support`}))
     }
 
 }
-
