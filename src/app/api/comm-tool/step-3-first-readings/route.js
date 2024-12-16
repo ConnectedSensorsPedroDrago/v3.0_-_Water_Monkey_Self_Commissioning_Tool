94cc477d25c70650ab71b6df68824966a158c7be
dragope <83094706+dragope@users.noreply.github.com>
2024-12-16 15:58:14 -0300

    132nd commit

--- src/app/api/comm-tool/step-3-first-readings/route.js
+++ src/app/api/comm-tool/step-3-first-readings/route.js
@@ -5,25 +5,7 @@ export async function POST(req){
     if(meterType === "Single" && lowSideFirst && dateFirst && lowSideFirstUnit && picFirst || meterType === "Compound" && lowSideFirst && highSideFirst && lowSideFirstUnit && highSideFirstUnit && dateFirst && picFirst){
             let newLowSideFirst = lowSideFirstUnit === "m3" ? lowSideFirst : lowSideFirstUnit === "liters" ? Number(lowSideFirst)*0.001 : lowSideFirstUnit === "gallons" && Number(lowSideFirst)*0.00378541
             let newHighSideFirst = highSideFirstUnit === "m3" ? highSideFirst : highSideFirstUnit === "liters" ? Number(highSideFirst)*0.001 : highSideFirstUnit === "gallons" && Number(highSideFirst)*0.00378541        
-        // try{
-        //     let payload = {"initial_meter_reading_primary": {"value": newLowSideFirst, "timestamp": dateFirst.timestamp, "context": {"pic": picURL, "date_time": dateFirst}}}
-        //     meterType === "Compound" && (payload = {"initial_meter_reading_primary": {"value": newLowSideFirst, "timestamp": dateFirst.timestamp, "context": {"pic": picURL, "date_time": dateFirst}}, "initial_meter_reading_secondary": {"value": newHighSideFirst, "timestamp": dateFirst.timestamp, "context": {"pic": picURL, "date_time": dateFirst}}})
 
-        //     let response = await fetch(`https://cs.api.ubidots.com/api/v1.6/devices/${params.id}/`, {
-        //         method: 'POST',
-        //         headers:{
-        //             'Content-Type':'application/json',
-        //             'X-Auth-Token': process.env.UBIDOTS_AUTHTOKEN,
-        //         },
-        //         body: JSON.stringify(payload)
-        //     })
-        //     let data = await response.json()
-        //     if(!data.initial_meter_reading_primary){
-        //         return new Response(JSON.stringify({"status": "error", "message": "There was an error writting the first readings. Please try again or contact support."}))
-        //     }
-        // }catch(e){
-        //     return new Response(JSON.stringify({"status": "error", "message": "There was an error writting the first readings: " + e + ". Please try again or contact support."}))
-        // }finally{
             try{
                 let payload = meterType === "Single" ? 
                     {"date_time": dateFirst, "low": lowSideFirst, "low_unit": lowSideFirstUnit, "pic": picURL}
@@ -67,7 +49,6 @@ export async function POST(req){
             }catch(e){
                 return new Response(JSON.stringify({"status": "error", "message": `There was an error writting the first readings: ${e}. Please try again or contact support.`}))
             }
-    //     }
     }else{
         return new Response(JSON.stringify({"status": "error", "message": "Please complete all the required fields to submit the first readings"}))
     }
